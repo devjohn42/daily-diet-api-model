29666a0595ac70e3659ab85892c9d694e0fcb46d
devjohn42 <devjohncb@gmail.com>
2025-04-09 00:25:00 -0300

    :wrench: knex setup

---
+++ src/database.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { Knex, knex as setupKnex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env not found.')
+}
+
+export const config: Knex.Config = ({
+  client: 'sqlite',
+  useNullAsDefault: true,
+  connection: {
+    filename: process.env.DATABASE_URL
+  },
+  migrations: {
+    extension: 'ts',
+    directory: './database/migrations'
+  }
+})
+
+export const knex = setupKnex(config)
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
-import { app } from "./app"
+import fastify from "fastify"
+import { randomUUID } from "node:crypto"
+import { knex } from "./database"
+
+const app = fastify()
+
+app.get('/diet', async () => {
+  const diet = await knex('diets').insert({
+    id: randomUUID(),
+    name: 'Test Daily Diet',
+    description: 'First diet of the day',
+    in_diet: true
+
+  })
+
+  return diet
+})
 
 app.listen({
   port: 3333
 }).then(() => {
-  console.log('HTTP Server Runing')
+  console.log('HTTP Server Running')
 })
