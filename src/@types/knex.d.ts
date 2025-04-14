394660fb14a158252c9d01c4c5cdb81cac0e0d1f
devjohn42 <devjohncb@gmail.com>
2025-04-14 16:32:56 -0300

    :bricks: defines another type in knex

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -2,11 +2,19 @@ import { Knex } from "knex";
 
 declare module 'knex/types/tables' {
   export interface Tables {
-    id: string
-    name: string
-    description: string
-    in_diet: boolean
-    created_at: string // Date
-    session_id?: string
+    users: {
+      id: string;
+      name: string;
+      email: string;
+      session_id: string;
+    }
+    meals: {
+      id: string
+      name: string
+      description: string
+      in_diet: boolean
+      created_at: string // Date
+      session_id?: string
+    }
   }
 }
