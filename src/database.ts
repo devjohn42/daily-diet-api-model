fa0aceb7ead3abe6f12bfd9d94da6052632fe7ec
devjohn42 <devjohncb@gmail.com>
2025-04-27 19:25:26 -0300

    :rocket: deployment configuration

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,20 @@
-import { Knex, knex as setupKnex } from 'knex'
-import { env } from './env'
+import { Knex, knex as setupKnex } from 'knex';
+import { env } from './env';
 
-export const config: Knex.Config = ({
-  client: 'sqlite',
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection: env.DATABASE_CLIENT === 'sqlite'
+    ? { filename: env.DATABASE_URL }
+    : env.DATABASE_URL,
   useNullAsDefault: true,
-  connection: {
-    filename: env.DATABASE_URL
-  },
   migrations: {
     extension: 'ts',
     directory: './database/migrations'
   }
-})
+}
+
+// console.log('Database Client:', env.DATABASE_CLIENT);
+// console.log('Database URL:', env.DATABASE_URL);
 
 export const knex = setupKnex(config)
 
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,13 +1,19 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
-// process.env
-// NODE_ENV: development | test | production
+if (process.env.NODE_ENV === 'test') {
+  config({
+    path: '.env.test'
+  })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333)
+  PORT: z.coerce.number().default(3333)
 })
 
 const _env = envSchema.safeParse(process.env)
